a34cee1e853054bd8144bbc9fc1b811601110ff7
Rémi Jacquemard <remi.jacquemard@heig-vd.ch>
2018-01-12 12:25:01 +0100

    login testing

---
+++ docs/script.js
@@ -0,0 +1,19 @@
+function makerandom() {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for (var i = 0; i < 30; i++)
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+}
+
+function setLoginLink() {
+    let randString = makerandom();
+
+    document.getElementById("loginLink")
+        .setAttribute("href", "https://www.wunderlist.com/oauth/authorize?client_id=706ea3bc47cae388cb26&redirect_uri=" 
+        + window.location.href + "&state=" + randString);
+}
+
+setLoginLink();
